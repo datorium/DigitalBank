f4032c0516b8e33ccdeff80fa7bbdd109800372a
Datorium <hello@datorium.lv>
2020-05-28 17:01:47 +0300

    version 17:01

--- BankAccount.cs
+++ BankAccount.cs
@@ -9,9 +9,11 @@ namespace DigitalBank
         public string Number { get; }
         public string Owner { get; set; }
         public decimal Balance { get; }
+        public DateTime DateCreated { get;}
 
         public BankAccount(string ownerName, decimal initialBalance)
         {
+            this.DateCreated = DateTime.Now;
             this.Number = "123456789";
             this.Owner = ownerName;
             this.Balance = initialBalance;
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace DigitalBank
         static void Main(string[] args)
         {
             BankAccount account = new BankAccount("Jenifer", 100);
-            Console.WriteLine($"Owner {account.Owner} has {account.Balance} euro(s) in account Nr. {account.Number}");
-        }
+            Console.WriteLine($"Owner {account.Owner} has {account.Balance} euro(s) in account Nr. {account.Number} created on {account.DateCreated.ToString("dd/MM/yyyy")}.");
+        }   
     }
 }
