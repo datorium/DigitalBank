1babd3c1a42ffd8cb0cfa7cd7275a8ed12ee2884
Datorium <hello@datorium.lv>
2020-05-29 16:31:18 +0300

    version 16:31

--- BankAccount.cs
+++ BankAccount.cs
@@ -8,8 +8,19 @@ namespace DigitalBank
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
-        public DateTime DateCreated { get;}
+        public decimal Balance         
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach(Transaction t in transactions)
+                {
+                    balance += t.Amount;
+                }
+                return balance;
+            }        
+        }
+        public DateTime DateCreated { get; }
 
         private List<Transaction> transactions = new List<Transaction>();
 
@@ -18,7 +29,7 @@ namespace DigitalBank
             this.DateCreated = DateTime.Now;
             this.Number = "123456789";
             this.Owner = ownerName;
-            this.Balance = initialBalance;
+            //this.Balance = initialBalance;
         }
 
         public void AddMoney(decimal amount, DateTime date, string notes)
